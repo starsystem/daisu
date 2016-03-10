3c3cbcd4497b680d4ff23cb5cae16089c987cb6a
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2016-03-11 00:16:06 +0100

    preview?

--- assets/js/orgs.js
+++ assets/js/orgs.js
@@ -59,6 +59,6 @@ function go(url){
     url,
     true
   );
-  xhrObject.setRequestHeader( 'Accept', 'application/vnd.github.raw+json' );
+  xhrObject.setRequestHeader( 'Accept', 'application/vnd.github.ironman-preview+json' );
   xhrObject.send();
 }
